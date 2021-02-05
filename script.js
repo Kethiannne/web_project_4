5decf86f43d7aae7e56f4ba82d555359330e6915
Kethiannne <natedadc@gmail.com>
2021-02-05 16:29:23 -0500

    fix edit form, tidied html
    edit form is now a form, submitting the form now changes the corresponding text and closes the form

--- script.js
+++ script.js
@@ -1,33 +1,19 @@
 let page = document.querySelector(".page");
-
 let profile = page.querySelector(".profile");
-
 let popup = page.querySelector(".popup");
-
 let edit = profile.querySelector(".profile__edit-button");
-
 let editForm = popup.querySelector(".edit-form");
-
 let closeEdit = editForm.querySelector(".edit-form__close-button");
-
-
-
-
-
-
 
 // A section For Opening and Closing the Popup
 //---------------------------------------------
 function openPopup(){
   popup.classList.add("popup_opened");
 }
-
 function closePopup(){
   popup.classList.remove("popup_opened");
 }
-
 edit.addEventListener("click", openPopup);
-
 closeEdit.addEventListener("click", closePopup);
 //----------------------------------------------
 
@@ -44,54 +30,45 @@ let likeButton6 = page.querySelector("#HB6");
 function like1(){
   likeButton1.classList.toggle("elements__heart_active");
 }
-
 function like2(){
   likeButton2.classList.toggle("elements__heart_active");
 }
-
 function like3(){
   likeButton3.classList.toggle("elements__heart_active");
 }
-
 function like4(){
   likeButton4.classList.toggle("elements__heart_active");
 }
-
 function like5(){
   likeButton5.classList.toggle("elements__heart_active");
 }
-
 function like6(){
   likeButton6.classList.toggle("elements__heart_active");
 }
-
 likeButton1.addEventListener("click", like1);
 likeButton2.addEventListener("click", like2);
 likeButton3.addEventListener("click", like3);
 likeButton4.addEventListener("click", like4);
 likeButton5.addEventListener("click", like5);
 likeButton6.addEventListener("click", like6);
-
 //---------------------------------------------
 
 
 // A section For the Edit Form Text
 //---------------------------------------------
 let nameForm = editForm.querySelector("#name-form");
-
 let occupationForm = editForm.querySelector("#occupation-form");
-
 let myName = profile.querySelector(".profile__name");
-
 let myOccupation = profile.querySelector(".profile__occupation");
-
 let saveButton = editForm.querySelector(".edit-form__save-button");
 
 function saveProfileEdits(){
   myName.textContent = nameForm.value;
   myOccupation.textContent = occupationForm.value;
+  closePopup();
+  event.preventDefault()
 }
 
+editForm.addEventListener("submit", saveProfileEdits);
 saveButton.addEventListener("click", saveProfileEdits);
-
 //---------------------------------------------
