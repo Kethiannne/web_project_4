a267b4e6c68c61a96694876e1082d94dc1def007
Kethiannne <natedadc@gmail.com>
2021-02-07 19:29:17 -0500

    Make edits to conform to Reviewer Guidelines
    Change icons to svg.
    Correct duplicate styles.
    Remove redundant/unused code.
    Removed set height for containers with text elements.
    Improved Button Formatting.
    Form Fields now Autofill on Open

--- script.js
+++ script.js
@@ -3,12 +3,19 @@ let profile = page.querySelector(".profile");
 let popup = page.querySelector(".popup");
 let edit = profile.querySelector(".profile__edit-button");
 let editForm = popup.querySelector(".edit-form");
-let closeEdit = editForm.querySelector(".edit-form__close-button");
+let closeEdit = popup.querySelector(".popup__close-button");
+
+let nameForm = editForm.querySelector(".edit-form__field_input_name");
+let occupationForm = editForm.querySelector(".edit-form__field_input_occupation");
+let myName = profile.querySelector(".profile__name");
+let myOccupation = profile.querySelector(".profile__occupation");
 
 // A section For Opening and Closing the Popup
 //---------------------------------------------
 function openPopup(){
   popup.classList.add("popup_opened");
+  nameForm.value = myName.textContent;
+  occupationForm.value = myOccupation.textContent;
 }
 function closePopup(){
   popup.classList.remove("popup_opened");
@@ -18,49 +25,9 @@ closeEdit.addEventListener("click", closePopup);
 //----------------------------------------------
 
 
-// A section For the Like Buttons
-//---------------------------------------------
-let likeButton1 = page.querySelector("#HB1");
-let likeButton2 = page.querySelector("#HB2");
-let likeButton3 = page.querySelector("#HB3");
-let likeButton4 = page.querySelector("#HB4");
-let likeButton5 = page.querySelector("#HB5");
-let likeButton6 = page.querySelector("#HB6");
-
-function like1(){
-  likeButton1.classList.toggle("elements__heart_active");
-}
-function like2(){
-  likeButton2.classList.toggle("elements__heart_active");
-}
-function like3(){
-  likeButton3.classList.toggle("elements__heart_active");
-}
-function like4(){
-  likeButton4.classList.toggle("elements__heart_active");
-}
-function like5(){
-  likeButton5.classList.toggle("elements__heart_active");
-}
-function like6(){
-  likeButton6.classList.toggle("elements__heart_active");
-}
-likeButton1.addEventListener("click", like1);
-likeButton2.addEventListener("click", like2);
-likeButton3.addEventListener("click", like3);
-likeButton4.addEventListener("click", like4);
-likeButton5.addEventListener("click", like5);
-likeButton6.addEventListener("click", like6);
-//---------------------------------------------
-
-
 // A section For the Edit Form Text
 //---------------------------------------------
-let nameForm = editForm.querySelector("#name-form");
-let occupationForm = editForm.querySelector("#occupation-form");
-let myName = profile.querySelector(".profile__name");
-let myOccupation = profile.querySelector(".profile__occupation");
-let saveButton = editForm.querySelector(".edit-form__save-button");
+
 
 function saveProfileEdits(){
   myName.textContent = nameForm.value;
@@ -70,5 +37,4 @@ function saveProfileEdits(){
 }
 
 editForm.addEventListener("submit", saveProfileEdits);
-saveButton.addEventListener("click", saveProfileEdits);
 //---------------------------------------------
